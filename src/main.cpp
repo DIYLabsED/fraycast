7c8b657c8aaab1107991b8ca843ee7c823b14e7d
DIYLabsED <DIYLabsED@users.noreply.github.com>
2026-06-02 22:08:54 +0530

    feat: render a window with SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
 int main(){
 
+  SDL_Window *window;
+
+  if(!SDL_Init(SDL_INIT_VIDEO)){
+    std::cerr << "SDL init failed with error " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  window = SDL_CreateWindow("Fraycast", 800, 600, 0);
+
+  if(window == NULL){
+    std::cerr << "SDL window init failed with error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Delay(5000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 
 }
